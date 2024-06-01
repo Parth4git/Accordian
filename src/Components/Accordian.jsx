713030d6e0b4017637ee160053ddb3235d032be5
Parth Dwivedi <99666524+Parth4git@users.noreply.github.com>
2024-06-01 12:37:32 +0530

    updated code

--- src/Components/Accordian.jsx
+++ src/Components/Accordian.jsx
@@ -1,35 +1,65 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import data from "./data.js";
 import "./Accordian.css";
 
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
+  const [enableMultiple, setEnableMultiple] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-  function handleClick(getDataid) {
-    if (selected === getDataid) {
-      return setSelected(null);
-    }
-    setSelected(getDataid);
-  }
+  const handleMultiple = (id) => {
+    setMultiple((prev) => {
+      if (prev.includes(id)) {
+        return prev.filter((item) => item !== id);
+      } else {
+        return [...prev, id];
+      }
+    });
+  };
+
+  const handleClick = (id) => {
+    setSelected((prev) => (prev === id ? null : id));
+  };
+
   return (
     <div>
-      <h1>Accordian</h1>
-      <div className="accordian">
+      <h1>Accordion</h1>
+      <button
+        onClick={() => setEnableMultiple(!enableMultiple)}
+        className="btn"
+      >
+        {enableMultiple
+          ? "Disable Multiple Questions"
+          : "Enable Multiple Questions"}
+      </button>
+      <div className="accordion">
         {data && data.length > 0 ? (
           data.map((item) => (
             <div key={item.id} className="item">
-              <h2>Question {item.id}</h2>
-              <div onClick={() => handleClick(item.id)} className="title">
-                <h3 className="question">{item.question}</h3>
-                <span className="icon">+</span>
+              <div
+                onClick={
+                  enableMultiple
+                    ? () => handleMultiple(item.id)
+                    : () => handleClick(item.id)
+                }
+                className="title"
+              >
+                <h3 className="question">
+                  Question {item.id}: {item.question}
+                </h3>
+                <span className="icon">
+                  {selected === item.id || multiple.includes(item.id)
+                    ? "-"
+                    : "+"}
+                </span>
               </div>
-
-              {selected === item.id ? <p>{item.answer}</p> : null}
+              {(selected === item.id || multiple.includes(item.id)) && (
+                <p>{item.answer}</p>
+              )}
             </div>
           ))
         ) : (
-          <div> "No data found" </div>
+          <div>No data found</div>
         )}
       </div>
     </div>
